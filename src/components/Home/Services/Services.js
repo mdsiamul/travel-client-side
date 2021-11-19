5077dc219b8094a34adc00d8f3bb359af4ef9cc1
mdsiamul <mdsiamulsuratprince@gmail.com>
2021-11-19 11:32:03 +0600

    add

--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -7,7 +7,7 @@ import './Services.css'
 const Services = () => {
     const [services, setServices] = useState([])
     useEffect(() => {
-        fetch('http://localhost:5000/packges')
+        fetch('https://secret-harbor-87106.herokuapp.com/packges')
             .then(res => res.json())
             .then(data => setServices(data));
     }, [])
